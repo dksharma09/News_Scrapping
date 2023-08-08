3f5d66e772481928a00d0400a943465ef2070ff8
dksharma09 <sharma.dushyantkumar1999@gmail.com>
2023-08-08 17:13:19 +0530

    Error Handled

--- scrp.py
+++ scrp.py
@@ -3,31 +3,29 @@ import requests
 import logging
 import time
 
-fileName = str(time.time())
+try :
+    fileName = str(time.time())
 
-logging.basicConfig(filename=f'logs/{fileName}.log', encoding='utf-8', level=logging.DEBUG)
+    logging.basicConfig(filename=f'logs/{fileName}.log', encoding='utf-8', level=logging.DEBUG)
 
-webData = requests.get("https://edition.cnn.com/travel/article/scenic-airport-landings-2020/index.html").text
-beautify = bs(webData, 'html.parser')
+    webData = requests.get("https://edition.cnn.com/travel/article/scenic-airport-landings-2020/index.html").text
+    beautify = bs(webData, 'html.parser')
 
-findHeading = beautify.find('div', {'class' : 'headline__wrapper'})
-heading = (findHeading.text).strip()
-logging.info(f'Title : {heading}')
+    findHeading = beautify.find('div', {'class' : 'headline__wrapper'})
+    heading = (findHeading.text).strip()
+    logging.info(f'Title : {heading}')
 
-findContent = beautify.find_all('p', {'class' : 'paragraph inline-placeholder'})
-print(len(findContent))
+    findContent = beautify.find_all('p', {'class' : 'paragraph inline-placeholder'})
+    print(len(findContent))
 
-# findHeader2 = beautify.find_all('h2', {'class' : 'subheader'})
-# print(len(findHeader2))
-# print(findHeader2[0].text)
-logging.info('Read the NEWS below : ')
-for i in range(len(findContent)) :
-    para = (findContent[i].text).strip()
-    logging.info(f'{para}')
+    # findHeader2 = beautify.find_all('h2', {'class' : 'subheader'})
+    # print(len(findHeader2))
+    # print(findHeader2[0].text)
+    logging.info('Read the NEWS below : ')
+    contentFile = ""
+    for i in range(len(findContent)) :
+        para = (findContent[i].text).strip()
+        logging.info(f'{para}')
 
-# heading = findHeading.text
-
-# Content = findContent.text
-# print(heading)
-# log heading
-# logging.info(f'Title : {Content}')
+except Exception as e :
+    logging.error(f'Error occurred : {e}')
